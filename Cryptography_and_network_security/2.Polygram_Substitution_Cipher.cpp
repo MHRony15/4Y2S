33b387055d5b5114805aaa9e3f2ac9c0cc04074b
MHRony15 <37172763+MHRony15@users.noreply.github.com>
2022-01-10 13:24:40 +0600

    Add files via upload

---
+++ Cryptography_and_network_security/2.Polygram_Substitution_Cipher.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+string PlaintextToCipher(string text)
+{
+	replace(text.begin(), text.end(), ' ', '_');
+
+	map <string, string> encryptDictionary;
+
+	ifstream readFile;
+
+	string msg, cipher;
+
+	readFile.open("block.txt");
+
+	if (readFile.is_open())
+	{
+		while(readFile >> msg >> cipher)
+		{
+			encryptDictionary[msg] = cipher;
+		}
+
+		readFile.close();
+	}
+	else
+	{
+		cout << "can not open the file" << endl;
+	}
+
+	string block;
+	int startPos = 0;
+
+	for(int i = 0; i < text.length(); i++)
+	{
+		block += text[i];
+
+		if(block.length() == 3)
+		{
+			block = encryptDictionary[block];
+
+			int x,y;
+
+			for(x = startPos, y = 0; y < 3 ; y++, x++)
+			{
+				text[x] = block[y];
+			}
+
+			startPos = x;
+
+			block.clear();
+		}
+	}
+
+	return text;
+}
+
+string CipherToPlaintext(string encrypt)
+{
+
+
+	map <string, string> decryptDictionary;
+
+	ifstream readFile;
+
+	string msg, cipher;
+
+	readFile.open("block.txt");
+
+	if (readFile.is_open())
+	{
+		while(readFile >> msg >> cipher)
+		{
+			decryptDictionary[cipher] = msg;
+		}
+
+		readFile.close();
+	}
+	else
+	{
+		cout << "can not open the file" << endl;
+	}
+
+	string block;
+	int startPos = 0;
+
+
+	for(int i = 0; i < encrypt.length(); i++)
+	{
+		block += encrypt[i];
+
+		if(block.length() == 3)
+		{
+			block = decryptDictionary[block];
+
+			int x,y;
+
+			for(x = startPos, y = 0; y < 3 ; y++, x++)
+			{
+				encrypt[x] = block[y];
+			}
+
+			startPos = x;
+
+			block.clear();
+		}
+	}
+
+	replace(encrypt.begin(), encrypt.end(), '_', ' ');
+
+	return encrypt;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	string text;
+
+	getline(cin, text);
+
+	cout << "......................" <<endl;
+
+	cout << "Plain Text: "<< text << endl << endl;
+
+	string encrypt = PlaintextToCipher(text);
+
+	cout <<"Cipher: " << encrypt << endl << endl;
+
+	string decrypt = CipherToPlaintext(encrypt);
+
+	cout << "Plain Text: "<< decrypt << endl;
+
+
+	return 0;
+}
